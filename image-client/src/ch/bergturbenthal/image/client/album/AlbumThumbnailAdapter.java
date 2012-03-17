18f136c512621e2fbdfa1d22d7c2f8cb1274a46f
Andreas Koenig <andreas.koenig@berg-turbenthal.ch>
2012-03-17 17:45:11 +0100

    optimized resource-usage

--- image-client/src/ch/bergturbenthal/image/client/album/AlbumThumbnailAdapter.java
+++ image-client/src/ch/bergturbenthal/image/client/album/AlbumThumbnailAdapter.java
@@ -12,12 +12,10 @@ import java.util.concurrent.Executors;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import ch.bergturbenthal.image.client.R;
 import ch.bergturbenthal.image.client.resolver.AlbumService;
 import ch.bergturbenthal.image.data.model.AlbumImageEntry;
 
@@ -25,7 +23,7 @@ public class AlbumThumbnailAdapter extends BaseAdapter {
 
   private final Context context;
   private final List<AlbumImageEntry> images = new ArrayList<AlbumImageEntry>();
-  private final ConcurrentMap<String, Drawable> cachedImages = new ConcurrentHashMap<String, Drawable>();
+  private final ConcurrentMap<String, File> cachedImages = new ConcurrentHashMap<String, File>();
   private AlbumService service = null;
   private final String albumId;
 
@@ -56,24 +54,17 @@ public class AlbumThumbnailAdapter extends BaseAdapter {
     ImageView imageView;
     if (convertView == null) {
       imageView = new ImageView(context);
-
-      // final int height = parent.getHeight();
-      // final int width = parent.getWidth();
-      // final int columnCount = width / 85;
-      // final int rowCount = height / 85;
-      // imageView.setLayoutParams(new GridView.LayoutParams(width /
-      // columnCount, height / rowCount));
       imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-      // imageView.setPadding(8, 8, 8, 8);
     } else {
       imageView = (ImageView) convertView;
     }
     final AlbumImageEntry imageEntry = images.get(position);
-    final Drawable foundDrawable = cachedImages.get(imageEntry.getId());
+    final File foundDrawable = cachedImages.get(imageEntry.getId());
     if (foundDrawable != null) {
-      imageView.setImageDrawable(foundDrawable);
-    } else
-      imageView.setImageDrawable(context.getResources().getDrawable(R.drawable.logo));
+      imageView.setImageDrawable(new BitmapDrawable(context.getResources(), foundDrawable.getAbsolutePath()));
+    }
+    // else
+    // imageView.setImageDrawable(context.getResources().getDrawable(R.drawable.logo));
     return imageView;
   }
 
@@ -87,7 +78,7 @@ public class AlbumThumbnailAdapter extends BaseAdapter {
         @Override
         public void run() {
           final File readImage = service.readImage(albumId, albumImageEntry.getId(), 128, 128);
-          cachedImages.put(albumImageEntry.getId(), new BitmapDrawable(context.getResources(), readImage.getAbsolutePath()));
+          cachedImages.put(albumImageEntry.getId(), readImage);
           activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
