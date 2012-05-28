43078a0643c9be6606c6ceca07e10f9c07191b88
Andreas Koenig <andreas.koenig@berg-turbenthal.ch>
2012-05-28 06:47:39 +0200

    some cleanups

--- image-client/src/ch/bergturbenthal/image/client/SelectServerListView.java
+++ image-client/src/ch/bergturbenthal/image/client/SelectServerListView.java
@@ -21,7 +21,7 @@ public class SelectServerListView extends ListActivity {
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    final ArrayAdapter<String> serverList = new ArrayAdapter<String>(this, R.layout.list_item);
+    final ArrayAdapter<String> serverList = new ArrayAdapter<String>(this, R.layout.server_list_item);
     setListAdapter(serverList);
     resolver = new Resolver(this);
     resolver.findServices(new Resolver.ServiceNameListener() {
