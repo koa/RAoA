45c5338f8513397325c4bdb6e11a14c60b88adec
akoenig <akoenig@uranus>
2012-11-04 09:19:57 +0100

    Cursor implementation extracted

--- image-provider/src/ch/bergturbenthal/image/provider/ArchiveContentProvider.java
+++ image-provider/src/ch/bergturbenthal/image/provider/ArchiveContentProvider.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
-import android.database.AbstractCursor;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
+import ch.bergturbenthal.image.provider.map.EntityCursor;
 import ch.bergturbenthal.image.provider.map.FieldReader;
 import ch.bergturbenthal.image.provider.map.MapperUtil;
 import ch.bergturbenthal.image.provider.map.StringFieldReader;
@@ -20,7 +20,6 @@ import ch.bergturbenthal.image.provider.orm.DatabaseHelper;
 import ch.bergturbenthal.image.provider.util.EnumUriMatcher;
 import ch.bergturbenthal.image.provider.util.Path;
 
-import com.j256.ormlite.dao.CloseableIterator;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
@@ -35,115 +34,7 @@ public class ArchiveContentProvider extends ContentProvider {
     THUMBNAIL_ENTRY
   }
 
-  private static class EntityCursor<V> extends AbstractCursor {
-    private final String[] queryingProjection;
-    private final QueryBuilder<V, String> queryBuilder;
-    private final FieldReader<V>[] fieldReaders;
-    private final CloseableIterator<V> dataIterator;
-    private V currentEntry = null;
-    private int calculatedCount = -1;
-
-    public EntityCursor(final String[] queryingProjection, final QueryBuilder<V, String> queryBuilder, final Map<String, FieldReader<V>> fieldReaders)
-                                                                                                                                                      throws java.sql.SQLException {
-      this.queryingProjection = queryingProjection;
-      this.queryBuilder = queryBuilder;
-      this.dataIterator = queryBuilder.iterator();
-      this.fieldReaders = new FieldReader[queryingProjection.length];
-      for (int i = 0; i < queryingProjection.length; i++) {
-        final FieldReader<V> fieldReader = fieldReaders.get(queryingProjection[i]);
-        if (fieldReader == null) {
-          throw new RuntimeException("no field Reader for field " + queryingProjection[i]);
-        }
-        this.fieldReaders[i] = fieldReader;
-      }
-    }
-
-    @Override
-    public String[] getColumnNames() {
-      return queryingProjection;
-    }
-
-    @Override
-    public synchronized int getCount() {
-      if (calculatedCount < 0) {
-        try {
-          calculatedCount = Integer.parseInt(queryBuilder.setCountOf(true).queryRawFirst()[0]);
-        } catch (final Throwable e) {
-          throw new RuntimeException("Cannot count results", e);
-        }
-        // TODO Auto-generated method stub
-      }
-      return calculatedCount;
-    }
-
-    @Override
-    public double getDouble(final int column) {
-      return getFieldReader(column).getNumber(currentEntry).doubleValue();
-    }
-
-    @Override
-    public float getFloat(final int column) {
-      return getFieldReader(column).getNumber(currentEntry).floatValue();
-    }
-
-    @Override
-    public int getInt(final int column) {
-      return getFieldReader(column).getNumber(currentEntry).intValue();
-    }
-
-    @Override
-    public long getLong(final int column) {
-      return getFieldReader(column).getNumber(currentEntry).longValue();
-    }
-
-    @Override
-    public short getShort(final int column) {
-      return getFieldReader(column).getNumber(currentEntry).shortValue();
-    }
-
-    @Override
-    public String getString(final int column) {
-      return getFieldReader(column).getString(currentEntry);
-    }
-
-    @Override
-    public int getType(final int column) {
-      final FieldReader<V> fieldReader = getFieldReader(column);
-      if (fieldReader.isNull(currentEntry))
-        return Cursor.FIELD_TYPE_NULL;
-      return fieldReader.getType();
-    }
-
-    @Override
-    public boolean isNull(final int column) {
-      return getFieldReader(column).isNull(currentEntry);
-    }
-
-    @Override
-    public boolean onMove(final int oldPosition, final int newPosition) {
-      if (oldPosition == newPosition)
-        return true;
-      if (oldPosition + 1 == newPosition) {
-        if (!dataIterator.hasNext())
-          return false;
-        currentEntry = dataIterator.next();
-        return true;
-      }
-      try {
-        currentEntry = dataIterator.moveRelative(newPosition - oldPosition);
-        return true;
-      } catch (final java.sql.SQLException e) {
-        Log.e(TAG, "Cannot move to " + newPosition, e);
-      }
-      return false;
-    }
-
-    private FieldReader<V> getFieldReader(final int column) {
-      return fieldReaders[column];
-    }
-  }
-
-  private static final String TAG = "Content Provider";
+  static final String TAG = "Content Provider";
 
   private static final EnumUriMatcher<UriType> matcher = new EnumUriMatcher<UriType>(Data.AUTHORITY, UriType.class);
   private final ThreadLocal<DaoHolder> transactionManager = new ThreadLocal<DaoHolder>() {
@@ -219,8 +110,7 @@ public class ArchiveContentProvider extends ContentProvider {
         // matrixCursor.addRow(new Object[] { albumEntity.getId(),
         // albumEntity.getName(), albumEntity.getArchive().getName() });
         // }
-        final CloseableIterator<AlbumEntity> dataIterator = queryBuilder.iterator();
-        return new EntityCursor(queryingProjection, queryBuilder, fieldReaders);
+        return new EntityCursor<AlbumEntity>(queryBuilder, queryingProjection, fieldReaders);
         // return matrixCursor;
 
       default:
