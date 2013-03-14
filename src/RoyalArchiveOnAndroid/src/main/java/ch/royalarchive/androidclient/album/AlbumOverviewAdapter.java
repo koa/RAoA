b0a8a7822e98f7f0a1ae1b4bf48291164fefec17
Andreas König <andreas.koenig@berg-turbenthal.ch>
2013-03-14 20:33:12 +0100

    Implemented enable of offline synchronization

--- src/RoyalArchiveOnAndroid/src/main/java/ch/royalarchive/androidclient/album/AlbumOverviewAdapter.java
+++ src/RoyalArchiveOnAndroid/src/main/java/ch/royalarchive/androidclient/album/AlbumOverviewAdapter.java
@@ -1,7 +1,10 @@
 package ch.royalarchive.androidclient.album;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.net.Uri;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.SimpleCursorAdapter;
 import ch.bergturbenthal.image.provider.Client;
@@ -20,17 +23,31 @@ public class AlbumOverviewAdapter extends SimpleCursorAdapter {
 		R.id.album_item_size, 
 		R.id.album_item_image };
 
+	private Context context;
+
 	public AlbumOverviewAdapter(Context context, int layout) {
 		super(context, layout, null, FROM, TO, 0);
-
+		this.context = context;
 		// set album overview view binder
 		setViewBinder(new OverviewBinder(false));
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View newView = super.getView(position, convertView, parent);
-		newView.setTag(getCursor().getString(getCursor().getColumnIndexOrThrow(Client.Album.ALBUM_ENTRIES_URI)));
+		ViewGroup newView = (ViewGroup) super.getView(position, convertView, parent);		
+		String childrenEntriesUri = getCursor().getString(getCursor().getColumnIndexOrThrow(Client.Album.ALBUM_ENTRIES_URI));
+		final String entryUri = getCursor().getString(getCursor().getColumnIndexOrThrow(Client.Album.ENTRY_URI));
+		newView.setTag(childrenEntriesUri);
+		View offlineIcon = newView.findViewById(R.id.album_item_icon_offline);
+		offlineIcon.setTag(entryUri);
+		offlineIcon.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ContentValues values=new ContentValues();
+				values.put(Client.Album.SHOULD_SYNC, Boolean.TRUE);
+				context.getContentResolver().update(Uri.parse(entryUri), values, null, null);
+			}
+		});
 		return newView;
 	}
 
