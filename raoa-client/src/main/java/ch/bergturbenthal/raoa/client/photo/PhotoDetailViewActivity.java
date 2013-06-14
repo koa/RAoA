4d3481d789789bdc7dfe8f6d67a1f7c29830e9d2
Andreas König <andreas.koenig@berg-turbenthal.ch>
2013-06-14 11:58:32 +0200

    Activated back-button

--- raoa-client/src/main/java/ch/bergturbenthal/raoa/client/photo/PhotoDetailViewActivity.java
+++ raoa-client/src/main/java/ch/bergturbenthal/raoa/client/photo/PhotoDetailViewActivity.java
@@ -30,6 +30,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -42,6 +43,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 import ch.bergturbenthal.raoa.R;
+import ch.bergturbenthal.raoa.client.album.AlbumOverviewActivity;
 import ch.bergturbenthal.raoa.client.binding.AbstractViewHandler;
 import ch.bergturbenthal.raoa.client.binding.CurserPagerAdapter;
 import ch.bergturbenthal.raoa.client.binding.PhotoViewHandler;
@@ -177,6 +179,26 @@ public class PhotoDetailViewActivity extends Activity {
 	public void onLowMemory() {
 		decrementOffscreenPageLimit();
 		super.onLowMemory();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			final Intent upIntent = new Intent(this, AlbumOverviewActivity.class);
+			upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(upIntent);
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
